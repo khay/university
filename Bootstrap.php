fbf1279c8bb027432a9747e486095b8a270d3e30
K <khayusaki@gmail.com>
2014-07-30 15:46:04 +0630

    Setup university module

--- Bootstrap.php
+++ Bootstrap.php
@@ -19,7 +19,7 @@ class Bootstrap extends \Reborn\Module\AbstractBootstrap
 	 */
 	public function adminMenu(\Reborn\Util\Menu $menu, $modUri)
 	{
-		$menu->add('university', 'University', $modUri, 'archive', 20);
+		$menu->add('university', 'University', $modUri, null, 'icon-college', 20);
 	}
 
 	/**
@@ -30,19 +30,13 @@ class Bootstrap extends \Reborn\Module\AbstractBootstrap
 	public function moduleToolbar()
 	{
 		return array(
-                'upload'    => array(
-                    'url'   => 'gallery',
-                    'name'  => 'All',
-                    'info'  => 'View all gallery lists',
-                    'id'    => 'gallery_lists',
-                ),
-                'create'    => array(
-                    'url'   => 'gallery/create/',
-                    'name'  => 'Create',
-                    'info'  => 'Create New Gallery',
-                    'id'    => 'create_gallery',
-                ),
-            );
+            'add'    => array(
+                'url'   => 'university/add/',
+                'name'  => 'Add',
+                'info'  => 'Add New University',
+                'id'    => 'addUniversity',
+            ),
+        );
 	}
 
 	/**
@@ -50,24 +44,7 @@ class Bootstrap extends \Reborn\Module\AbstractBootstrap
 	 *
 	 * @return array
 	 */
-	public function settings()
-	{
-		return array(
-			'gallery_image_width' => array(
-				'type' => 'text'
-				),
-			'gallery_image_height'	=> array(
-				'type' => 'text'
-				),
-			'gallery_photowall_zoomsize' => array(
-				'type' => 'text'
-				),
-			'gallery_photowall_use_social' => array(
-					'type'	=> 'select',
-					'options' => array('enable' => 'Enable', 'disable'=>'Disable')
-				)
-			);
-	}
+	public function settings() {}
 
 	/**
 	 * Register method for Module.
@@ -75,25 +52,6 @@ class Bootstrap extends \Reborn\Module\AbstractBootstrap
 	 * You can register at requirement for Reborn CMS.
 	 *
 	 */
-	public function register()
-	{
-		// Bind \Media\Api for Controller Injection
-		$this->app->bind('\Media\Api', function(){
-			return new \Media\Api();
-		});
-
-		// Define Gallery Asset Path
-		if (!defined('GALLERY_ASSET')) {
-			define('GALLERY_ASSET', __DIR__.DS.'assets'.DS);
-		}
-
-		// Define Gallery View Path
-		if (!defined('GALLERY_VIEW')) {
-			define('GALLERY_VIEW', __DIR__.DS.'views'.DS);
-		}
-
-		// Make Class Alias
-        \Alias::aliasRegister(array('Gallery' => 'Gallery\Facade\Gallery'));
-	}
+	public function register() {}
 
 }
